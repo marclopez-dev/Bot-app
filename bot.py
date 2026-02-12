ebd429caa70bfb18468555e1a5450a2a7dec1fe7
marclopez-dev <lopezpontemarc@gmail.com>
2026-02-12 12:54:53 -0500

    Add new route for '/PAG1' to render inicio.html

--- bot.py
+++ bot.py
@@ -3,5 +3,8 @@ app=Flask(__name__)
 @app.route("/")
 def on():
     return render_template("index.html")
+@app.route("/PAG1")
+def pop()
+    return render_template("inicio.html")
 if __name__=="__main__":
     app.run(debug=True)
