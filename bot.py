a027e5ec1f75564bf95d1f10b2a34e3995af65ee
Marc Lopez <marclopez-dev>
2026-02-12 00:25:43 -0500

    Guardando cambios locales

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
-from flask import Flask
-import render_template
+from flask import Flask, render_template
 app=Flask(__name__)
-@app.route("/n")
+@app.route("/")
 def on():
     return render_template("index.html")
 if __name__=="__main__":
