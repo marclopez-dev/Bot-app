4470805b86b4e40dc24c2f87fb2cc39faa0c8e25
marclopez-dev <lopezpontemarc@gmail.com>
2026-02-16 14:02:57 -0500

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -9,5 +9,16 @@ def pop():
 @app.route("/Pag2")
 def one():
     return render_template("limork.html")
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+@app.route("/mensaje")
+methods = ["POST"]
+def mensaje():
+    datos_recibidos = request.json
+    texto = datos_recibidos["mensaje"]
+    mensaje_respuesta = f"Hola, escribiste {texto}, te doy la bienvenida"
+    return  jsonify({"respuesta: ", mensaje_respuesta})
+    
 if __name__=="__main__":
     app.run(debug=True)
