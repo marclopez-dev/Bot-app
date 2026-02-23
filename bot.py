0b79aa9413af9cf4c211bfc6bfedba34485a17ba
marclopez-dev <lopezpontemarc@gmail.com>
2026-02-23 16:15:01 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///usuarios.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+class Usuarios(db.Model):
+    num_usu = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+with app.app_context():
+    db.create_all()
 @app.route("/")
 def on():
     return render_template("index.html")
