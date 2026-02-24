02422163ab221ebf266f93d91354503d5e7f7022
marclopez-dev <lopezpontemarc@gmail.com>
2026-02-24 14:44:42 -0500

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///usuario.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL" )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 class Usuario(db.Model):
