38cca6977d30b076de2aa51b56ea60fd38942011
marclopez-dev <lopezpontemarc@gmail.com>
2026-03-08 00:37:30 -0500

    Crear chat.js

---
+++ chat.js
@@ -0,0 +1,15 @@
+const {Client, localAuth} = require('whatsapp-web.js');
+const QRCode = require('qrcode');
+const Client = Client({
+     authStrategy: new LocalAuth();
+});
+client.on("qr", async qr => {
+    const qrImage = await QRCode.toDataUrl(qr);
+    await fetch("/qr_generate", {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json"},
+        body: JSON.stringify({qr: qrImage});
+});
+});
+client.initialize();
