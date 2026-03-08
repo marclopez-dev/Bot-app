f9742b13dfdf184952e24d33def30107fca9f2a6
marclopez-dev <lopezpontemarc@gmail.com>
2026-03-08 13:53:09 -0500

    Actualizar chat.js

--- chat.js
+++ chat.js
@@ -6,6 +6,7 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         executablePath: "/usr/bin/chromium",
+        headless: true,
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox"
