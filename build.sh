9f2e13d21f6de4c7c0bb21ccdc6e9f9d3429a5ec
marclopez-dev <lopezpontemarc@gmail.com>
2026-03-13 02:04:20 -0500

    Actualizar build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Instalar dependencias de Node.js
+npm install
+
+# Instalar ffmpeg (sí se puede)
 apt-get update
 apt-get install -y ffmpeg
-npm install
+
+# Instalar yt-dlp para el usuario
+python3 -m pip install --upgrade yt-dlp --user
