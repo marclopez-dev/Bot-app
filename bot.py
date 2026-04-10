8fea395904c1773be9eff5c57fc5170297f05ac7
marclopez-dev <lopezpontemarc@gmail.com>
2026-04-09 20:36:33 -0500

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -28,11 +28,10 @@ def responder(usuar, pregunta):
                 {
                     "role": "system",
                     "content": "Eres un asistente conversacional natural. "
-                        "Habla de forma relajada y fluida. ".
+                        "Habla de forma relajada y fluida y si te preguntan por tu creador o quién te programó responde con el nombre de Marc. ".
                         "Responde como en una conversación normal entre personas. "
                         "Puedes hacer preguntas cortas para continuar la charla."
                         "No uses mucho texto si no es necesario."
-                        "Si te preguntan por tu creador o quién te programó, responde con el nombre de Marc."
                 }
             ]
             + historial +
