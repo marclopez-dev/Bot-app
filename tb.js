57419ecad0879823b9a6faf8e4636c71ebe121dd
marclopez-dev <lopezpontemarc@gmail.com>
2026-06-10 07:01:24 -0500

    Actualizar tb.js

--- tb.js
+++ tb.js
@@ -302,18 +302,17 @@ if (name.toLowerCase().startsWith(".face")) {
 }
 ////
 
-let clave = msg.key.fromMe || msg.key.participantPn || msg.key.senderPn || msg.key.remoteJid;
-let OWNER = ["51940006397@s.whatsapp.net",
-"51982752625@s.whatsapp.net"]
+let clave = msg.key.participantPn || msg.key.senderPn;
+let OWNER = ["51940006397@s.whatsapp.net"]
 ///////////////////
 if (mens.startsWith(">∆")) {
  await sock.sendPresenceUpdate("composing", from);
       await new Promise(r => setTimeout(r, 1000));
       try{
-         //if (!OWNER.includes(clave)) {
-        // await sock.sendMessage(from, //{text: `acceso no autorizado para ${from}//`})
-        // return;
-         //}
+         if (!OWNER.includes(clave) || !msg.key.fromMe) {
+        await sock.sendMessage(from, {text: `acceso no autorizado para ${from}`})
+         return;
+         }
           
      
          const txt = mens.replace(/^\>∆\s*/, "");
